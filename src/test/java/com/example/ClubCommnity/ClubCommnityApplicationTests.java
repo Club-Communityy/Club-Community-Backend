8c76cb4d634763660963c04442e4e3e6ff770480
김형준 <gudwns051613@gmail.com>
2024-06-08 18:21:39 +0900

    first commit

---
+++ src/test/java/com/example/ClubCommnity/ClubCommnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClubCommnity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubCommnityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
