436147b0d959a8a17492230f29e0f67ecf5d04c3
pakyoong <phb4101@gmail.com>
2024-06-10 19:34:44 +0900

    [ADD] Cors 설정

--- src/main/java/com/example/ClubCommunity/Config/WebConfig.java
+++ src/main/java/com/example/ClubCommunity/Config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.ClubCommunity.Club.Config;
+package com.example.ClubCommunity.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
