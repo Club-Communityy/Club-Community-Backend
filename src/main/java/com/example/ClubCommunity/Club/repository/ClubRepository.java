e6a4cbc4733b3a9635989078c38d9de93e6656cc
pakyoong <phb4101@gmail.com>
2024-06-11 14:22:57 +0900

    [UPDATE] 동아리 조회 관련 수정

--- src/main/java/com/example/ClubCommunity/Club/repository/ClubRepository.java
+++ src/main/java/com/example/ClubCommunity/Club/repository/ClubRepository.java
@@ -1,6 +1,7 @@
 package com.example.ClubCommunity.Club.repository;
 
 import com.example.ClubCommunity.Club.domain.Club;
+import com.example.ClubCommunity.Member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
     List<Club> findByStatus(Club.ClubStatus status);
+    List<Club> findByApplicant(Member applicant);
 }
